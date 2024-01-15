644c91ea27ad236f1ccb596c7c15965fc5def96f
PyjamaMole <pyjamamole@gmail.com>
2024-01-16 00:43:58 +0100

    new fix for lighting issues

--- src/main/java/protosky/gen/WorldGenUtils.java
+++ src/main/java/protosky/gen/WorldGenUtils.java
@@ -4,11 +4,8 @@ import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.registry.entry.RegistryEntry;
-import net.minecraft.server.world.ServerLightingProvider;
-import net.minecraft.server.world.ServerWorld;
 import net.minecraft.util.collection.PackedIntegerArray;
 import net.minecraft.util.math.BlockPos;
-import net.minecraft.util.math.ChunkSectionPos;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.world.*;
 import net.minecraft.world.biome.Biome;
@@ -20,6 +17,7 @@ public class WorldGenUtils
 {
     public static void deleteBlocks(ProtoChunk chunk)
     {
+        // remove blocks
         ChunkSection[] sections = chunk.getSectionArray();
         for (int i = 0; i < sections.length; i++) {
             ChunkSection chunkSection = sections[i];
@@ -27,6 +25,8 @@ public class WorldGenUtils
             ReadableContainer<RegistryEntry<Biome>> biomeContainer = chunkSection.getBiomeContainer();
             sections[i] = new ChunkSection(blockStateContainer, biomeContainer);
         }
+
+        // remove block entities
         for (BlockPos bePos : chunk.getBlockEntityPositions())
         {
             chunk.removeBlockEntity(bePos);
@@ -42,40 +42,15 @@ public class WorldGenUtils
 
     }
 
-    public static void genHeightMaps(ProtoChunk chunk, ServerLightingProvider lightingProvider)
+    public static void genHeightMaps(ProtoChunk chunk)
     {
 
-        //Tried to fix the lighting issue in a more elegant way but have yet to succeed, in the mean time this quick and dirty solution will
-        //update both the light levels and the heightmap.
-
-        /*
+        // set heightmaps
         int elementBits = MathHelper.ceilLog2(chunk.getHeight() + 1);
         long[] emptyHeightmap = new PackedIntegerArray(elementBits, 256).getData();
         for (Map.Entry<Heightmap.Type, Heightmap> heightmapEntry : chunk.getHeightmaps())
         {
             heightmapEntry.getValue().setTo(chunk, heightmapEntry.getKey(), emptyHeightmap);
-        }
-
-        for (int i = chunk.getBottomSectionCoord(); i < chunk.getTopSectionCoord(); i++){
-            lightingProvider.enqueueSectionData(LightType.SKY, ChunkSectionPos.from(chunk.getPos(), i), new ChunkNibbleArray(15));
-        }
-        */
-
-        int count = 0;
-        int z = chunk.getTopY()-1;
-        while (count <= 255) {
-            int x = count / 16;
-            int y = count % 16;
-            chunk.setBlockState(new BlockPos(x, z, y), Blocks.STONE.getDefaultState(), false);
-            chunk.setBlockState(new BlockPos(x, z, y), Blocks.AIR.getDefaultState(), false);
-
-            //lightingProvider.setSectionStatus(new BlockPos(x, z, y), false);
-            //lightingProvider.setSectionStatus(new BlockPos(x, z, y), true);
-            //chunk.getChunkSkyLight().isSkyLightAccessible(chunk, x, z, y);
-            //lightingProvider.checkBlock(new BlockPos(x, z, y));
-
-
-            count++;
         }
 
     }
--- src/main/java/protosky/mixins/ChunkStatusMixin.java
+++ src/main/java/protosky/mixins/ChunkStatusMixin.java
@@ -1,9 +1,10 @@
 package protosky.mixins;
 
+import com.mojang.datafixers.util.Either;
+import net.minecraft.server.world.ChunkHolder;
 import net.minecraft.server.world.ServerLightingProvider;
 import net.minecraft.server.world.ServerWorld;
 import net.minecraft.structure.StructureTemplateManager;
-import net.minecraft.world.Heightmap;
 import net.minecraft.world.chunk.Chunk;
 import net.minecraft.world.chunk.ChunkStatus;
 import net.minecraft.world.chunk.ProtoChunk;
@@ -11,28 +12,41 @@ import net.minecraft.world.gen.chunk.ChunkGenerator;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import protosky.gen.WorldGenUtils;
 
-import java.util.EnumSet;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.function.Function;
 
+
+
 @Mixin(ChunkStatus.class)
 public abstract class ChunkStatusMixin
 {
-    // LIGHT
-    @Inject(method = "method_20614", at = @At("HEAD"))
-    //This is really not called 'onLighting' It's called 'GenerationTask'
-    private static void onLighting(ChunkStatus targetStatus, Executor executor, ServerWorld world, ChunkGenerator generator, StructureTemplateManager structureTemplateManager, ServerLightingProvider lightingProvider, Function fullChunkConverter, List chunks, Chunk chunk, CallbackInfoReturnable<CompletableFuture> cir)
+    // inserting skyblock code when chunk gets lit
+    @Inject(method = "getInitializeLightingFuture", at = @At("HEAD"))
+    private static void getInitializeLightingFuture(ServerLightingProvider lightingProvider, Chunk chunk, CallbackInfoReturnable<CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> cir)
     {
-        if(!chunk.getStatus().isAtLeast(targetStatus)) {
-            //Heightmap.populateHeightmaps(chunk, EnumSet.of(Heightmap.Type.MOTION_BLOCKING, Heightmap.Type.MOTION_BLOCKING_NO_LEAVES, Heightmap.Type.OCEAN_FLOOR, Heightmap.Type.WORLD_SURFACE));
-            WorldGenUtils.deleteBlocks((ProtoChunk) chunk);
-            WorldGenUtils.clearEntities((ProtoChunk) chunk);
-            WorldGenUtils.genHeightMaps((ProtoChunk) chunk, lightingProvider);
-        }
+        WorldGenUtils.deleteBlocks((ProtoChunk) chunk);
+        //WorldGenUtils.clearEntities((ProtoChunk) chunk);
+        WorldGenUtils.genHeightMaps((ProtoChunk) chunk);
     }
+
+    @Inject(method = "getLightingFuture", at = @At("HEAD"))
+    private static void getLightingFuture(ServerLightingProvider lightingProvider, Chunk chunk, CallbackInfoReturnable<CompletableFuture<Either<Chunk, ChunkHolder.Unloaded>>> cir)
+    {
+        WorldGenUtils.deleteBlocks((ProtoChunk) chunk);
+        //WorldGenUtils.clearEntities((ProtoChunk) chunk);
+        WorldGenUtils.genHeightMaps((ProtoChunk) chunk);
+    }
+
+    // cancelling initial spawns of entities (not including those in structures)
+    @Inject(method = "method_17033", at = @At("HEAD"), cancellable = true)
+    private static void SPAWN(ChunkStatus targetStatus, ServerWorld world, ChunkGenerator generator, List chunks, Chunk chunk, CallbackInfo ci) {
+        ci.cancel();
+    }
+
 }
